93840dc32763ae985e488d7aa1b54b9398b24362
toxikmusic <153168348+toxikmusic@users.noreply.github.com>
2026-02-18 07:12:36 -0800

    Update auth-page.tsx

--- client/src/pages/auth-page.tsx
+++ client/src/pages/auth-page.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { insertUserSchema } from "@shared/schema";
+import { insertUserSchema } from "@/shared/schema";
 import { useAuth } from "@/hooks/use-auth";
 import { Redirect } from "wouter";
 import { Button } from "@/components/ui/button";
